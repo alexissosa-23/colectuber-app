d176f29187aab817a4121b8c741700ec8fb2026c
alexissosa-23 <alexis.sosa@fiuni.edu.py>
2022-04-14 21:29:28 -0400

    Correr la aplicacion en el background.
    Logre  hacer que la App pueda seguir haciendo post al backend a pesar de que la aplicación este en segundo plano.

--- App.js
+++ App.js
@@ -1,22 +1,91 @@
 import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Platform, StyleSheet, Text, View, Button, Alert } from 'react-native';
-import * as Location from 'expo-location';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 import Api from './api';
+import * as Location from "expo-location"
+import * as TaskManager from "expo-task-manager"
 
-const colectivoId = 3;
+
+const TASK_NAME = "BACKGROUND_LOCATION_TASK"
+const choferId = 19;
 let mensaje = "Activar Ubicacion"
 let text = 'Waiting..';
+//const [ubicacion, setUbicacion] = useState(null);
+
+TaskManager.defineTask(TASK_NAME, async ({ data, error }) => {
+  if (error) {
+    console.error(error);
+  }
+  if (data) {
+
+    let location = await Location.getCurrentPositionAsync({});
+    text = JSON.stringify(location);
+    let datos = {
+      posicionColectivo: {
+        latitud: location.coords.latitude,
+        longitud: location.coords.longitude
+      },
+      chofer_id: choferId
+    }
+    console.log("Post desde el segundo plano ",datos)
+
+  }
+});
+const isTrackingLocation = async () => {
+  let hasStarted = await Location.hasStartedLocationUpdatesAsync(TASK_NAME);
+  return hasStarted;
+}
+
+const requestPermissionsAsync = async () => {
+  const foregroundPermission = await Location.requestForegroundPermissionsAsync();
+  if (!foregroundPermission.granted) return false;
+  const backgroundPermission = await Location.requestBackgroundPermissionsAsync();
+  if (!backgroundPermission.granted) return false;
+  return true;
+}
+
+const startLocationTracking = async () => {
+  let permissions = await requestPermissionsAsync();
+  if (!permissions) throw new Error("Permission not granted.");
+  if (!TaskManager.isTaskDefined(TASK_NAME)) throw new Error("Task not ready or defined.");
+  let hasStarted = await isTrackingLocation();
+  if (hasStarted) throw new Error("Tracking already started.")
+
+  await Location.startLocationUpdatesAsync(TASK_NAME, {
+    accuracy: Location.Accuracy.BestForNavigation,
+    showsBackgroundLocationIndicator: true,
+    foregroundService: {
+      notificationTitle: "Location",
+      notificationBody: "Location tracking in background",
+      notificationColor: "#fff",
+    }
+  });
+}
+const stopLocationTracking = async () => {
+  let hasStarted = await isTrackingLocation();
+  if (hasStarted) {
+    await Location.stopLocationUpdatesAsync(TASK_NAME);
+  } else {
+    throw new Error("Tracking not started.")
+  }
+}
+
+
 export default function App() {
+
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
   const [activo, setAtivo] = useState(false)
-  function postUbicacion(){
+  const [act, setAct] = useState(false);
+  const [ready, setReady] = useState(false);
+
+  // obtener la ubicacion y hacer post
+  function postUbicacion() {
     (async () => {
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== 'granted') {
         setErrorMsg('Permission to access location was denied');
-       // return;
+        // return;
       }
 
       let location = await Location.getCurrentPositionAsync({});
@@ -29,32 +98,34 @@ export default function App() {
       }
 
       let datos = {
-      colectivoId: colectivoId,
-      posicionColectivo: {
-        latitud: location.coords.latitude,
-        longitud: location.coords.longitude
+        posicionColectivo: {
+          latitud: location.coords.latitude,
+          longitud: location.coords.longitude
+        },
+        chofer_id: choferId
       }
-    }
-    console.log(datos)
+      console.log(datos)
 
-    let res = await Api.post("colectuber/ubicacion", datos)
+      let res = await Api.post("colectuber/ubicacion", datos)
 
     })();
   };
 
+  // para el intervalo de 2.5 segundos
   useEffect(() => {
     if (activo) {
       const interval = setInterval(() => {
         postUbicacion();
-      }, 2500);
+      }, 2000);
       return () => {
         clearInterval(interval)
       }
     }
   }, [activo])
 
+  // para cambiar el estado de activo y el mensaje en el boton
   function ubicacionActDes() {
-    if(!activo){
+    if (!activo) {
       Alert.alert(
         'ColectuberApp',
         'Desea activar su Ubicacion',
@@ -69,12 +140,13 @@ export default function App() {
             onPress: () => {
               mensaje = "Desactivar Ubicacion"
               setAtivo(!activo)
+              activate()
             },
           },
         ],
 
       )
-    }else{
+    } else {
       Alert.alert(
         'ColectuberApp',
         'Desea desactivar su Ubicacion',
@@ -89,6 +161,7 @@ export default function App() {
             onPress: () => {
               mensaje = "Activar Ubicacion"
               setAtivo(!activo)
+              desactivate()
             },
           },
         ],
@@ -98,12 +171,43 @@ export default function App() {
 
   }
 
+  const checkTracking = async () => {
+    let hasStarted = await isTrackingLocation();
+    setAct(hasStarted);
+    setReady(true);
+  }
+
+  useEffect(() => {
+    checkTracking();
+  }, []);
+
+  function activate() {
+    setReady(false);
+    startLocationTracking()
+      .then(() => {
+        setAct(true);
+        console.log("Activated");
+      })
+      .catch((err) => console.error(err))
+      .finally(() => setReady(true))
+  }
+
+  function desactivate (){
+    setReady(false);
+    stopLocationTracking()
+      .then(() => {
+        setAct(false);
+        console.log("Deactivated");
+      })
+      .catch((err) => console.error(err))
+      .finally(() => setReady(true))
+  }
   return (
     <View style={styles.container}>
       <Text>ColectuberApp!</Text>
       <StatusBar style="auto" />
       <Button
-         onPress={() => {
+        onPress={() => {
           ubicacionActDes()
         }}
         backgroundColor='#0000fff'
@@ -122,3 +226,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
