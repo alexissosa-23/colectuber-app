e544777cc46b1bb3349d2c72c4837e02629852b0
JoseSosa96 <85372100+JoseSosa96@users.noreply.github.com>
2022-05-02 17:12:11 -0400

    login
    login

---
+++ src/components/home/home.jsx
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import { View, Text, Button, StyleSheet, Dimensions } from 'react-native';
+import { useAuthContext } from 'src/contexts/auth-context-provider';
+import ColectuberService from 'src/services/colectuber-service';
+import ActivationButton from './activation-button';
+
+var { height } = Dimensions.get('window');
+
+var box_count = 3;
+var box_height = height / box_count;
+var meta ='jgcjgcj';
+
+
+const Home = () => {
+
+
+
+    const authContext = useAuthContext();
+    return <View style={styles.container}>
+        <View style={[styles.box, styles.box1]}>
+            <Text style={styles.containerText}>
+                Home
+            </Text>
+        </View>
+        <View style={[styles.box, styles.box2]}>
+            <Text style={styles.containerText}>
+                ColectuberApp
+            </Text>
+            <ActivationButton />
+            <Button title='Log Out' onPress={authContext.logout}/>
+            <Text style={styles.containerText}>
+                Destino (viaje):
+            </Text>
+            <Text>{JSON.stringify(authContext.viaje)}</Text>
+        </View>
+        <View style={[styles.box, styles.box3]}></View>
+    </View>
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+    },
+    box: {
+        height: box_height
+    },
+    //header
+    box1: {
+        flex: 1.2,
+        backgroundColor: '#e3aa1a',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    //content
+    box2: {
+        flex: 10,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    //footer
+    box3: {
+        flex: .5,
+        backgroundColor: '#e3aa1a'
+    },
+    containerText: {
+        paddingTop: 15,
+        fontSize: 20,
+        textShadowColor: '#000000'
+    },
+});
+export default Home;
---
+++ src/components/login/login.jsx
@@ -0,0 +1,78 @@
+
+import React,{useState} from 'react';
+import { useAuthContext } from 'src/contexts/auth-context-provider';
+import BordeSuperior from './BordeSuperior.jsx';
+//import LogoInicioSesion from './LogoInicioSesion.jsx';
+//import Titulo from './Titulo.jsx';
+
+const Login = () => {
+    const authContext = useAuthContext();
+    const [name, setName] = useState("");
+    const [pass, setPass] = useState("");
+    const [ready, setReady] = useState(true);
+
+//Autenticacion
+    const login = () => {
+        if(!name || !pass){
+            return
+        }
+        setReady(false)
+        authContext.login(name, pass)
+            .catch(err=>{
+                console.error(err);
+                //Contrasena o username equivocado o error al autenticar
+                setReady(true);
+            })
+    }
+
+
+        return (
+         
+              <Text>Login</Text>
+
+        );
+      }
+    
+
+           //   <BordeSuperior/>
+            /*<KeyboardAvoidingView>
+              
+               
+                <View>
+                    <TextInput
+                        style={{ height: 50, width: 300, borderBottomColor: '#000000', borderBottomWidth: 1, paddingTop: 20, }}
+                        value={name}
+                        placeholder={'Usuario o Correo Electronico '}
+                        onChangeText={newName => setName(newName)}
+                    />
+                </View>
+                <View>
+                    <TextInput
+                        style={{ height: 50, width: 300, borderBottomColor: '#000000', borderBottomWidth: 1, marginTop: 40, paddingTop: 20, }}
+                        value={pass}
+                        placeholder={'Contraseña'}
+                        secureTextEntry={true}
+                        onChangeText={newPass => setPass(newPass)}
+                    />
+    </View>
+            </KeyboardAvoidingView>
+
+          {/*  <View style={styles.container2}>
+                <Button
+                    disabled = {!ready}
+                    style={{ height: 45, width: 200, marginTop: 60 }}
+                    backgroundColor='rgb(255, 127, 39)'
+                    color='rgb(255, 127, 39)'
+                    borderColor='rgb(255, 127, 39)'
+                    borderBottomColor='rgb(0, 127, 39)'
+                    onPress={login}
+                    title='INICIAR SESION'
+
+                />
+            </View>*/
+
+
+
+
+
+export default Login;
