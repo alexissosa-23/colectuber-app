aa7973855ba4468c85a86a35235d3f857d58c6aa
JoseSosa96 <85372100+JoseSosa96@users.noreply.github.com>
2022-05-02 20:44:41 -0400

    Login
    Login

--- App.js
+++ App.js
@@ -1,12 +1,99 @@
-import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
-import React, { Component } from 'react';
-import BordeSuperior from './components/BordeSuperior';
-export default class App extends Component {
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
+export default function App() {
+  return (
+    <View style={styles.container}>
 
-  render() {
-    return (
-         <BordeSuperior/>
-    );
-  }
+      <View style={styles.bordeSuperior}>
+      </View>
+
+      <View style={styles.titulo}>
+      <Image style={styles.titulo2} source={require("./assets/titulo.png")}/>
+      </View>
+
+      <View style={styles.logo}>
+        <Image style={styles.logo2} source={require("./assets/logocolectivo.png")}/>
+      </View>
+
+      <View style={styles.correo}>
+        <Text>Correo Electronico</Text>
+      </View>
+
+      <View style={styles.pass}>
+        <Text>Contraseña</Text>
+      </View>
+      <View style={styles.boton}>
+        <Text>Iniciar Sesion</Text>
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    height: '100%',
+    width: '100%',
+    // flexDirection: 'column', 
+  },
+  bordeSuperior: {
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '10%',
+    width: '100%',
+  },
+  titulo: {
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '8%',
+    width: '100%',
+  },
+  titulo2: {
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '80%',
+    width: '80%',
+  },
+  logo: {
+    backgroundColor: 'gray',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '30%',
+    width: '100%',
+  },
+  logo2: {
+    backgroundColor: 'gray',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '90%',
+    width: '60%',
+  },
+  correo: {
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '10%',
+    width: '100%',
+    borderColor: '#000',
+  },
+  pass: {
+    backgroundColor: 'gray',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '10%',
+    width: '100%',
+    borderColor: '#000',
+  },
+  boton: {
+    backgroundColor: 'orange',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '10%',
+    width: '100%',
+    borderColor: '#000',
+  },
+});
--- api.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-const Api = axios.create({
-    baseURL:"https://colectuberbackenddeploy.azurewebsites.net/api/"
-})
-export default Api;
